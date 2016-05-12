6f3ce4733bafe91d1e87a6c9d7184189b1cf4b52
Joboomafoo <jingram2@engr-131-14.eastern.ewu.edu>
2016-05-12 14:41:42 -0700

    Trying again

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('article').readmore({
+  speed: 500,
+  collapsedHeight: 190,
+  moreLink: '<a href="#">OH SNAP!</a>',
+lessLink: '<a href="#">BE GONE!</a>'
+});
--- js/scripts.js
+++
@@ -1,2 +0,0 @@
-// My Scripts
-$('p').readmore();
